77a8b926ed2a4ae022d82a81715c6b0ab32f2de2
ansh <anshsingh25bd@gmail.com>
2025-07-04 21:44:47 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import Index from "./pages/Index";
 
 const App = () => {
-  // Allow cell navigation with keyboard
+
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       // Prevent default scrolling behavior when arrow keys are pressed
       if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Tab"].includes(e.key)) {
-        // Only prevent if focus is in the spreadsheet
+   
         if (document.activeElement?.closest('.bg-[#F6F6F6]')) {
           e.preventDefault();
         }
